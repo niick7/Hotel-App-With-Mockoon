02e0ce95e473d417f16b0320a3f61ce3a1f5577e
Nhan Vo <niick7@gmail.com>
2024-11-26 14:38:21 -0600

    Using the localStorage in the reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,26 +7,37 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
+  // Constructor will beloaded before onInit life cycle hook
+  constructor() {
+    let savedReservations = localStorage.getItem('reservations');
+    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
+  }
+
   // CRUD
   getReservations(): Reservation[] {
     return this.reservations;
   }
 
   getReservation(id: string): Reservation | undefined {
-    return this.reservations.find(res => res.id === id);
+    return this.reservations.find((res) => res.id === id);
   }
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
+    localStorage.setItem('reservation', JSON.stringify(this.reservations));
   }
 
   updateReservation(updatedReservation: Reservation): void {
-    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    let index = this.reservations.findIndex(
+      (res) => res.id === updatedReservation.id
+    );
     this.reservations[index] = updatedReservation;
+    localStorage.setItem('reservation', JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
-    let index = this.reservations.findIndex(res => res.id === id);
+    let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations.splice(index, 1);
+    localStorage.setItem('reservation', JSON.stringify(this.reservations));
   }
 }
